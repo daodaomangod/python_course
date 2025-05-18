0c9c27e1f24199e9c242bf85dd157253e615f110
daodao <1212121@qq.com>
2025-05-18 08:25:38 +0800

    提交作业

--- day2/4functionAndModule.py
+++ day2/4functionAndModule.py
@@ -1,4 +1,5 @@
 import math
+import random
 
 
 # 定义的函数
@@ -14,5 +15,17 @@ def count_x(x):
     return y
 
 
-x = eval(input("请输入x的值:"))
-print(f"输入值:{x},函数的计算成果{count_x(x):.3f}")
+# x = eval(input("请输入x的值:"))
+# print(f"输入值:{x},函数的计算成果{count_x(x):.3f}")
+
+"""
+ramdom.seed()
+ramdom.random()
+描述
+初始化随机数种子，默认为当前系统时间。
+生成一个[0 , 1)之间的随机小数。
+>>> ramdom.random()
+random.uniform(a,b)生成一个[a , b]之间的随机小数。
+ramdom.randint(a,b)生成一个[a , b]之间的随机整数。
+"""
+
